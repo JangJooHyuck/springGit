3b4f38d229226fb8827545691197a9f28a33df68
JangJooHyuck <unodos4@infobank.net>
2021-07-20 09:56:00 +0900

    0955

--- demo/src/main/java/com/example/demo/controller/ApiEmailcheck.java
+++ demo/src/main/java/com/example/demo/controller/ApiEmailcheck.java
@@ -1,13 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.stereotype.Controller;
+
+
 @Controller
 public class ApiEmailcheck {
-    @RequestMapping("/api/")
+    
+    @RequestMapping("/")
     public String root_test() throws Exception{
-        return "index";
+        return "EmailCheck";
     }
  
     @RequestMapping("/demo")
