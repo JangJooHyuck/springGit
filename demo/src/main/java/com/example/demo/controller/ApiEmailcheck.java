936da431bb10fd06224c671d2971d9e386ce635a
JangJooHyuck <unodos4@infobank.net>
2021-07-20 09:07:11 +0900

    0720

--- demo/src/main/java/com/example/demo/controller/ApiEmailcheck.java
+++ demo/src/main/java/com/example/demo/controller/ApiEmailcheck.java
@@ -4,15 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 @Controller
-public class test {
-    @RequestMapping("/2")
-   
+public class ApiEmailcheck {
+    @RequestMapping("/api/")
     public String root_test() throws Exception{
         return "index";
     }
  
     @RequestMapping("/demo")
-    
     public String demo_test() throws Exception{
         return "Hello demo(/demo)";
     }
