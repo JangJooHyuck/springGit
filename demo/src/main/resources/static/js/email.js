867045e79a9c3b2b10a3ef5d943edf9d0a86e580
JangJooHyuck <unodos4@infobank.net>
2021-07-21 15:12:17 +0900

    1512

--- demo/src/main/resources/static/js/email.js
+++ demo/src/main/resources/static/js/email.js
@@ -8,10 +8,11 @@ function verifyEmail() {
      // id=result 안에 innerText를 통해 저장된 email 의 값을 출력해줌
      document.getElementById("inputEmail").innerText = email;
      
+     const 
      fetch('http://localhost:1233/api')
      .then(console.log(email))
 
-     if()
+   
         
          }
      
