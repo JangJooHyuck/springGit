64706e1da55d34d198493e370fe6f3d4777754ce
JangJooHyuck <unodos4@infobank.net>
2021-07-22 00:15:31 +0900

    0722_1215

--- demo/src/main/java/com/example/demo/controller/ApiController.java
+++ demo/src/main/java/com/example/demo/controller/ApiController.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@RequestMapping("/api")
+@RequestMapping("/api")1
 public class ApiController {
 
     @Autowired
