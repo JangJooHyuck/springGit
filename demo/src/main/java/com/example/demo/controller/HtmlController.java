72657e8ed05cd27d18ea849250335b1edc2d79e6
JangJooHyuck <unodos4@infobank.net>
2021-07-22 15:41:41 +0900

    0722_1541

--- demo/src/main/java/com/example/demo/controller/HtmlController.java
+++ demo/src/main/java/com/example/demo/controller/HtmlController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
