391a27c41e8593315b9202eb9ad9d14a53e27e2f
JangJooHyuck <unodos4@infobank.net>
2021-08-05 00:23:18 +0900

    0805_header 적용

--- demo/src/main/java/com/example/demo/controller/ApiController.java
+++ demo/src/main/java/com/example/demo/controller/ApiController.java
@@ -11,23 +11,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
-
-
 import com.example.demo.model.Userlog;
 import com.example.demo.service.EmailcheckService;
 
 import java.util.List;
 
-
-
-
 @Controller
-//  /api로 들어오는것들은 밑에 클래스에서 받아올수있다.
+// /api로 들어오는것들은 밑에 클래스에서 받아올수있다.
 @RequestMapping("/api")
 public class ApiController {
 
-    //객체 자동생성 Autowired
+    // 객체 자동생성 Autowired
     @Autowired
     EmailcheckService emailcheckservice;
 
@@ -36,55 +30,59 @@ public class ApiController {
 
     Userlog userlog = new Userlog();
 
-    //get방식
+    // get방식
     @GetMapping("/get")
     @ResponseBody
     // 값이 check 인것을 파라미터로 받아온다.
-    public Userlog getResult(@RequestParam(value = "check") String userEmail, @RequestParam(value="userIP") String UserIP ) throws Exception {
+    public Userlog getResult(@RequestParam(value = "check") String userEmail,
+            @RequestParam(value = "userIP") String UserIP) throws Exception {
 
-        //3초 지연
+        // 3초 지연
         Thread.sleep(3000);
         Boolean usermailboolean = emailcheckservice.isValidEmail(userEmail);
         userlog.setDate(dbservice.findNow());
-        userlog.setIdx(dbservice.findIdx()+1);
+        userlog.setIdx(dbservice.findIdx() + 1);
         userlog.setResult(dbservice.logBoolean(usermailboolean));
         userlog.setMail(userEmail);
         userlog.setUserIP(UserIP);
-        //db에 로그 저장
+        // db에 로그 저장
         dbservice.insertLog(userlog);
         return userlog;
     }
 
-    //post 방식
+    // post 방식
     @RequestMapping(value = "/post", method = RequestMethod.POST)
     @ResponseBody
     public Userlog POSTresult(@RequestBody Userlog userlog) throws Exception {
 
-        //3초 지연
+        // 3초 지연
         Thread.sleep(3000);
 
-        Boolean usermailboolean = emailcheckservice.isValidEmail(userlog.getResult());
+        Boolean usermailboolean = emailcheckservice.isValidEmail(userlog.getMail());
         userlog.setDate(dbservice.findNow());
-        userlog.setIdx(dbservice.findIdx()+1);
+        userlog.setIdx(dbservice.findIdx() + 1);
         userlog.setResult(dbservice.logBoolean(usermailboolean));
-123123
-        //db에 로그 저장
+        userlog.setMail(userlog.getMail());
+
+        // db에 로그 저장
         dbservice.insertLog(userlog);
         return userlog;
 
     }
-     // select * from log order by idx desc limit start , 10
-     @GetMapping("/ajax")
-     @ResponseBody
-     public List<Userlog> getLog(@RequestParam(value = "pageNumber" , defaultValue = "1") int pageNumber) throws Exception{
-         return dbservice.getAllLog((pageNumber-1) * 10, 10);
-     }
 
-     // total page 찾아주는 api 현재 databse count 에서 보여줄 목록(10)을 나누고 나머지가 > 0 +1 or 10 나눈그대로 리턴
-     @GetMapping("/findIdxCount")
-     @ResponseBody
-     public int getCount(){
-         return dbservice.findIdx()%10 > 0 ? dbservice.findIdx()/10 + 1 : dbservice.findIdx()/10;
-     }
+    // select * from log order by idx desc limit start , 10
+    @GetMapping("/ajax")
+    @ResponseBody
+    public List<Userlog> getLog(@RequestParam(value = "pageNumber", defaultValue = "1") int pageNumber)
+            throws Exception {
+        return dbservice.getAllLog((pageNumber - 1) * 10, 10);
+    }
+
+    // total page 찾아주는 api 현재 databse count 에서 보여줄 목록(10)을 나누고 나머지가 > 0 +1 or 10
+    // 나눈그대로 리턴
+    @GetMapping("/findIdxCount")
+    @ResponseBody
+    public int getCount() {
+        return dbservice.findIdx() % 10 > 0 ? dbservice.findIdx() / 10 + 1 : dbservice.findIdx() / 10;
+    }
 }
-
