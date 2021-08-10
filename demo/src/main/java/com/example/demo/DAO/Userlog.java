c9a4c95f77ba8e2dbc2e70977f96eb4f48d24ab3
JangJooHyuck <unodos4@infobank.net>
2021-08-10 22:27:59 +0900

    0810_주석추가

--- demo/src/main/java/com/example/demo/DAO/Userlog.java
+++ demo/src/main/java/com/example/demo/DAO/Userlog.java
@@ -14,13 +14,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.persistence.Table;
 
+// jpa 는 데이터베이스에 작성되는 테이블을 다루기 위해
+// Entity라는 클래스를 사용함
+
 @Getter
 @NoArgsConstructor
 @Entity
 @Table(name = "userlog")
+//TimeEntity 상속 date 관련
 public class Userlog extends TimeEntity {
 
+    //기본키
     @Id
+    //기본키 생성을 데이터베이스에 위임 (IDENTITY (default 값은 auto))
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
--- demo/src/main/java/com/example/demo/DAO/UserlogRepository.java
+++ demo/src/main/java/com/example/demo/DAO/UserlogRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+                                                //<사용하고자 하는 엔티티,식별자타입>
 public interface UserlogRepository extends JpaRepository<Userlog, Long> {
 
     Userlog findFirstByOrderByIdDesc();
